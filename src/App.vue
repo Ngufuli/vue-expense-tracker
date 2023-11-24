3c00b5ff012eca1237f1745aeeb3fb46ce30992a
Ngufuli <ngufuli@outlook.com>
2023-11-24 10:08:07 +0530

    txn lst

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
   import Header from './components/Header.vue';
   import Balance from './components/Balance.vue';
+  import IncomeExpense from './components/IncomeExpense.vue';
 </script>
 
 <template>
   <Header />
   <div class="container">
     <Balance />
+    <IncomeExpense />
   </div>
 </template>
