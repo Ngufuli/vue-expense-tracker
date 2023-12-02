526694caa8dd46068df75c59c2dc77596e4ee895
Ngufuli <ngufuli@outlook.com>
2023-12-02 16:05:58 +0530

    transaction array list in global state

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,14 @@
   import IncomeExpense from './components/IncomeExpense.vue';
   import TransactionList from './components/TransactionList.vue';
   import AddTransactionVue from './components/AddTransaction.vue';
+  import {ref} from 'vue';
+
+  const transactions = [
+        {id: 1, text: 'Flower', amount: -19.00},
+        {id: 2, text: 'Salary', amount: 299.97},
+        {id: 3, text: 'Book', amount: -10},
+        {id: 4, text: 'Camera', amount: 150}
+    ]
 </script>
 
 <template>
