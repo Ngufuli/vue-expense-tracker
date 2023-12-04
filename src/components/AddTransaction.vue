918459bf22b8472e8fcefae236841d37f25d5ae8
Ngufuli <ngufuli@outlook.com>
2023-12-04 15:32:25 +0530

    preventDefault and data binding

--- src/components/AddTransaction.vue
+++ src/components/AddTransaction.vue
@@ -1,13 +1,19 @@
+<script setup>
+    const onSubmit = () => {
+        console.log('submit')
+    }
+</script>
+
 <template>
     <h3>Add new transaction</h3>
     <form id="form">
-        <div class="form-control">
+        <div class="form-control" @submit.prevent="onSubmit">
             <label for="text">Text</label>
-            <input type="text" id="text" placeholder="Enter text..."/>
+            <input type="text" id="text" placeholder="Enter text..." v-model="text"/>
         </div>
         <div class="form-control">
             <label for="amount">Amount <br/>(negative - expense, positive - income)</label>
-            <input type="text" id="amount" placeholder="Enter amount..."/>
+            <input type="text" id="amount" placeholder="Enter amount..." v-model="amount"/>
         </div>
         <button class="btn">Add transaction</button>
     </form>
